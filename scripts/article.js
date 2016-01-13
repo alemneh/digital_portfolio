88cba779174ee39b22095779b6120775e45cdb2c
Alemneh <Alemneh@hotmail.com>
2016-01-12 17:07:23 -0800

    Added ajax and JSON to portfolio and added ETag with a conditional

--- scripts/article.js
+++ scripts/article.js
@@ -8,38 +8,53 @@ function Example (opts) {
 Example.all = [];
 
 Example.prototype.toHtml = function() {
+
   var template = Handlebars.compile($('#portfolioData').text());
 
+  // this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  // this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  // this.description = marked(this.description);
+
   return template(this);
+
 };
 
 Example.loadAll = function(rawData) {
-  rawData.forEach(function(ele) {
-    Example.all.push(new Example(ele));
+  Example.all = rawData.map(function(ele) {
+    return new Example(ele);
+  });
+
+};
+
+Example.getAll = function () {
+  $.getJSON('/data/portfolioExamples.json', function(rawData) {
+    console.log('else worked');
+    Example.loadAll(rawData);
+    localStorage.rawData = JSON.stringify(rawData);
+    articleView.initIndexPage();
+
   });
 };
 
 Example.fetchAll = function() {
   if(localStorage.rawData) {
-    Example.loadAll(JSON.parse(localStorage.rawData));
-    articleView.initIndexPage();
-
-  } else {
-    $.getJSON('/data/portfolioExampoles.json', function(rawData) {
-      Example.loadAll(rawData);
-      localStorage.rawData = JSON.stringify(rawData);
-      articleView.initIndexPage();
+    var xhr = $.ajax({
+      type: 'HEAD',
+      url: '/data/portfolioExamples.json',
+      success: function(data, message, xhr) {
+        var serverEtag = xhr.getResponseHeader('eTag');
+        if(!localStorage.eTag || localStorage.eTag !== serverEtag ) {
+          localStorage.eTag = serverEtag;
+          Example.getAll();
+        } else {
+          Example.loadAll(JSON.parse(localStorage.rawData));
+          articleView.initIndexPage();
+        }
+      }
     });
 
+  } else {
+    Example.getAll();
+
   }
-};
-
-
-
-
-var dataSource = {
-  title: this.title,
-  imageUrl: this.imageUrl,
-  link: this.link,
-  description: this.description
 };
--- scripts/articleView.js
+++ scripts/articleView.js
@@ -1,7 +1,7 @@
 var articleView = {};
 
 articleView.initIndexPage = function() {
-  Article.all.forEach(function(a){
+  Example.all.forEach(function(a){
     $('#portfolio').append(a.toHtml());
   });
 };
